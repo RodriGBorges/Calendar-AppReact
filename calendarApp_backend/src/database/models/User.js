d0f1d9a81bd813d89a110963567894f1db82b32c
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-25 22:48:19 -0300

    Creacion del modelo usuarios

---
+++ calendarApp_backend/src/database/models/User.js
@@ -0,0 +1,20 @@
+// Para crear los modelos Schema y model
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model('User', UserSchema);
