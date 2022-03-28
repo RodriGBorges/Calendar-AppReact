5fce59a1e2cb84038a10e1955effb6425cc64c94
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-28 16:52:41 -0300

    Controlador pedido de todos los eventos

--- calendarApp_backend/src/controllers/eventController.js
+++ calendarApp_backend/src/controllers/eventController.js
@@ -3,6 +3,23 @@ const Event = require('../database/models/Event');
 module.exports = {
     all: async (req, res) => {
 
+        try {
+            
+            const events = await Event.find().populate('user', 'name');
+
+            return res.status(200).json({
+                ok: true,
+                data: events
+            })
+
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                ok: false,
+                msg: 'Contacte con el administrador del sitio.',
+                error
+            })
+        }
     },
     create: async (req, res) => {
 
