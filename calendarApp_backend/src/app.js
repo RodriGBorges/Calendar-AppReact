817048c203d5073c9ab8a96b7e3fba7e07364011
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-29 22:47:18 -0300

    Se modifica el render de error por json para mejor manejo de backend

--- calendarApp_backend/src/app.js
+++ calendarApp_backend/src/app.js
@@ -43,8 +43,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+  res.status(err.status || 500).json({
+    ok: false,
+    msg: err.message
+  });
 });
 
 module.exports = app;
