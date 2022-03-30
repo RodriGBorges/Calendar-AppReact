744a601b7b562b600d9ab38a783accb39fc049f2
RodriGBorges <gabii.borges1998@gmail.com>
2022-03-29 23:14:23 -0300

    Funcion de actualizar evento

--- calendarApp_backend/src/controllers/eventController.js
+++ calendarApp_backend/src/controllers/eventController.js
@@ -48,6 +48,46 @@ module.exports = {
     },
     update: async (req, res) => {
 
+        const { id } = req.params;
+
+        try {
+
+            const event = await Event.findById(id);
+
+            if(!event) {
+                return res.status(404).json({
+                    ok: false,
+                    msg: 'El evento no existe.'
+                })
+            }
+            // El id del usuario logueado debe coincidir con el id del usuario que creó el evento
+            if(event.user.toString() !== req.uid) {
+                return res.status(401).json({
+                    ok: false,
+                    msg: 'No tiene los permisos para editar este evento.'
+                })
+            }
+
+            const eventUpdate = {
+                ...req.body,
+                user: req.uid
+            }
+            // findByIdAndUpdate: buscá por id y actualizá el evento - Más sencillo que sequelize
+            await Event.findByIdAndUpdate(id, eventUpdate);
+
+            return res.status(200).json({
+                ok: true,
+                msg: 'El evento fue actualizado con éxito.'
+            })
+
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                ok: false,
+                msg: 'Contacte con el administrador del sitio.',
+                error
+            })
+        }
     },
     remove: async (req, res) => {
 
